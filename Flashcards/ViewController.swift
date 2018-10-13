886d863a1c715d88ebc5c585b20621bf5866b192
Me <caramariesebastian@icloud.com>
2018-10-13 15:36:42 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Cara Marie Sebastian-Ramos on 10/13/18.
+//  Copyright © 2018 Cara Marie Sebastian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
