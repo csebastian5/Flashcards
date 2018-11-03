77e687f0362bb1b4c6b262637cf5460a37493deb
Me <caramariesebastian@icloud.com>
2018-11-03 14:14:43 -0500

    update

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -18,13 +18,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
-
-
-
-
-
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         frontLabel.isHidden = true
+        
     }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
+    }
+    
+    func updateFlashcard(question: String, answer: String) {
+        dismiss(animated: true)
+    }
+    
 }
+
+
